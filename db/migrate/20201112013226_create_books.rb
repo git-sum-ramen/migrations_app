c6b0c74cdfa6301ffd73eb9d666aa15d07d227e3
brian <riceb53@gmail.com>
2020-11-11 20:33:22 -0500

    initial commit, generate model

---
+++ db/migrate/20201112013226_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :desctiption
+      t.string :isbn
+
+      t.timestamps
+    end
+  end
+end
